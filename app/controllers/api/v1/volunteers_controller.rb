81f8d243bd439423c76921ad1f54c9761ddbb2d0
Eric Zell <EricJZell@gmail.com>
2016-08-16 23:31:50 -0400

    refine endpoint to update volunteers status

--- app/controllers/api/v1/volunteers_controller.rb
+++ app/controllers/api/v1/volunteers_controller.rb
@@ -1,8 +1,32 @@
 class Api::V1::VolunteersController < ApplicationController
   protect_from_forgery except: :report_status
+  before_filter :parse_request, :authenticate_user_from_token!
 
   def report_status
-    binding.pry
+    detected_ips = @json['detected_ip_addresses']
+    detected_ips.each do |ip|
+      vol = Volunteer.find_by_ip_address(ip)
+      if vol
+        vol.last_detected = Time.now
+        if !vol.save!
+          render nothing: true, status: 500
+          return
+        end
+      end
+    end
+    render json: {processed_ip_addresses: detected_ips}, status: 200
   end
+
+private
+
+   def authenticate_user_from_token!
+     if @json['token'] != "Hello"
+       render nothing: true, status: :unauthorized
+     end
+   end
+
+   def parse_request
+     @json = JSON.parse(request.body.read)
+   end
 
 end
