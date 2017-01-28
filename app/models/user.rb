c969b14a76de104ac2f2e3285407f65e541bcc24
Eric Zell <EricJZell@gmail.com>
2017-01-28 16:57:19 -0500

    Add a name field to user sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :email, presence: true
+  validates :name, presence: true
   has_many :volunteers, -> { order(last_detected: :desc) }, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
